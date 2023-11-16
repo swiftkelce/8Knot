ccd7a4df655e81866d811e31079790f50f397666
Sean P. Goggins <s@goggins.com>
2023-11-16 14:54:26 -0600

    adding more vizes to a page
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- 8Knot/pages/chaoss_1/chaoss_1.py
+++ 8Knot/pages/chaoss_1/chaoss_1.py
@@ -6,6 +6,8 @@ import warnings
 # import visualization cards
 from .visualizations.tt_first_response import gc_tt_first_response
 from .visualizations.bus_factor import gc_bus_factor_pie
+from .visualizations.change_req_closure_ratio import gc_cr_closure_ratio
+from .visualizations.release_frequency import gc_release_frequency 
 
 warnings.filterwarnings("ignore")
 
@@ -17,6 +19,14 @@ layout = dbc.Container(
             [
                 dbc.Col(gc_bus_factor_pie, width=6),
                 dbc.Col(gc_tt_first_response, width=6),
+            ],
+            align="center",
+            style={"marginBottom": ".5%"},
+        ),
+        dbc.Row(
+            [
+                dbc.Col(gc_cr_closure_ratio, width=6),
+                dbc.Col(gc_release_frequency, width=6),
             ],
             align="center",
             style={"marginBottom": ".5%"},
